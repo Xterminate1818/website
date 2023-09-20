de195b96029afbb2780fd1f6ac7c46316cdb2bb0
Logan <logan@gatlintc.com>
2023-09-20 07:13:01 -0500

    AST rework, working state now

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use yew::prelude::*;
+#![feature(fn_traits)]
+use leptos::*;
 
-#[function_component(App)]
-fn app() -> Html {
-  html! {
-    <h1>{ "Logan Gatlin" } </h1>
+#[component]
+fn App() -> impl IntoView {
+  let (read, write) = create_signal(0);
+
+  view! {
+      <button on:click=move |_| write.update(|n| *n += 1)>
+          "Click me: "
+          {move || {read()}}
+      </button>
+      <p>"hello"</p>
   }
 }
 
 fn main() {
-  yew::Renderer::<App>::new().render();
+  mount_to_body(|| view! {<App/>})
 }
