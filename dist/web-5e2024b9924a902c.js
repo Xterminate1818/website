74eb028bdd239664414d8358c6dcb16b5e8681e3
Logan <50534996+Xterminate1818@users.noreply.github.com>
2023-10-16 01:02:18 -0500

    release build

--- dist/web-5e2024b9924a902c.js
+++ dist/web-5e2024b9924a902c.js
@@ -992,7 +992,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-b462f44b0206105b_bg.wasm', import.meta.url);
+        input = new URL('web-5e2024b9924a902c_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
